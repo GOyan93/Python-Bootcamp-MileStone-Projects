ba6d44a1fd4c271f7e63300bb3160f08734a6a19
Greg <goyandle@gmail.com>
2020-05-01 13:04:16 -0400

    Add board + printBoard function

---
+++ TicTacToe.py
@@ -0,0 +1,20 @@
+#TicTacToe Game
+#python 3.6.9
+#Author:GOyan
+
+theBoard = {'7': '', '8': '', '9': '',
+            '4': '', '5': '', '6': '',
+            '1': '', '2': '', '3': ''}
+
+
+
+def printBoard(board):
+    print(board['7'] + '|' + board['8'] + '|' + board['9'] + '\n' +
+          '-' + '+' + '-' + '+' + '-' + '\n' +
+          board['4'] + '|' + board['5'] + '|' + board['6'] + '\n' +
+          '-' + '+' + '-' + '+' + '-' + '\n' +
+          board['1'] + '|' + board['2'] + '|' + board['3'])
+
+printBoard(theBoard)
+          
+    
