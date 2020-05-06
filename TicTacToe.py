1ed26994b52cdb77e0672a3add254958f65bce7e
Greg <goyandle@gmail.com>
2020-05-06 16:05:20 -0400

    Add minor bug fix experiments (Unsuccessful)

--- TicTacToe.py
+++ TicTacToe.py
@@ -21,11 +21,11 @@ def printBoard(board):
           board['1'] + '|' + board['2'] + '|' + board['3'])
 
 def playerONEchoice(playerONEinput):
-    theBoard[playerONEinput] = player1
+    theBoard[playerONEinput.upper()] = player1
     printBoard(theBoard)
     
 def playerTWOchoice(playerTWOinput):
-    theBoard[playerTWOinput] = player2
+    theBoard[playerTWOinput.upper()] = player2
     printBoard(theBoard)
 
 def win():
@@ -73,14 +73,12 @@ def winConditions():
 
 # Main game loop. Loop does not stop after win!
 def mainGame():
-    while winConditions() == False:
+    
         print('PLAYER 1: Choose your position.')
         playerONEInput = input()
         playerONEchoice(playerONEInput.upper())
 
-        if winConditions() == True:
-            break
-            win()
+        
         print('PLAYER 2: Choose your position.')
         playerTWOInput = input()
         playerTWOchoice(playerTWOInput.upper())
@@ -88,10 +86,17 @@ def mainGame():
 def replay():
     print('Would you like to play again? (Y/N)')
     yesNo = input()
-    if yesNo.upper() == 'Y': 
-        return True
-    return False
-        
+    if str(yesNo) == 'Y' or 'y': 
+        theBoard = {'7': ' ', '8': ' ', '9': ' ',
+            '4': ' ', '5': ' ', '6': ' ',
+            '1': ' ', '2': ' ', '3': ' '}
+        winConditions = False
+        while winConditions() == False:
+            mainGame()
+            if winConditions() == True:
+                win()
+    else:
+        sys.exit()
         
 #TODO Add introduction and rules.
 printBoard(theBoard)
@@ -102,13 +107,16 @@ player1 = input()
 
 if player1.upper() == 'X':
     player2 = 'O'
-    
+    print('Player 1: X, Player 2: O')
 elif player1.upper() == 'O':
     player2 = 'X'
-    print('Player 1: X, Player 2: O')
-
-
+    print('Player 1: O, Player 2: X')
+    
+while winConditions() == False:
     mainGame()
-    win()
-    replay()
+    if winConditions() == True:
+        win()
+        replay()
+    else:
+        continue
 
