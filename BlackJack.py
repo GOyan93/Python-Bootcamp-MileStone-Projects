c0584336c1d5f8fe1164e82b3d2e01bf838d87f2
Greg <goyandle@gmail.com>
2020-05-09 18:29:54 -0400

    Added general layout and plan comments.

--- BlackJack.py
+++ BlackJack.py
@@ -2,3 +2,49 @@
 # Author: GOyan
 # Description: A basic blackjack style game based on OOP in python.
 # Python 3.6 
+
+
+# Game Layout
+    # Asks player for bet amount
+    # Deals comp 2 cards (only 1 shows), player 2 cards
+    # Asks Player to hit or stand
+        # After player stand, dealer rules come into play
+    # Sums comp hand and sums player hand
+        # If either over 21, bust and other wins
+        # If both bust, comp wins
+    # Compares comp sum and player sum
+        # Greater or equal is player win
+
+
+
+
+# Create deck class
+    # Full deck function
+    # removal of cards in deck function
+        # Must be randomly drawn
+    # reset deck function
+    # if sum is greater than 21, Ace = 1, else equals 11
+
+# Create bank class
+    # base amount to start bank
+    # withdrawal function for betting
+    # deposit function for wins
+    # Alert function for when withdrawing more than bank amount
+    # Alert function for when bank = 0
+
+# Create win / loss check function
+
+# Create bust check function
+
+
+# Create a while loop to keep playing
+    # if money  = 0, stop playing
+    # if player inputs certain key, stops playing
+
+
+    
+# Dealer rules:
+    # When the player has played:
+        # The second card is shown.
+        # If dealer <= 16, the dealer must draw
+        # If dealer >= 17, the dealer must stand
