ec604f4637ccda76661332017fd82bc63ee5fdf3
Greg <goyandle@gmail.com>
2020-05-09 20:55:02 -0400

    Added player class, and draw methods

--- BlackJack.py
+++ BlackJack.py
@@ -56,8 +56,30 @@ class Deck:
         for i in range(len(self.cards)-1, 0, -1):
             r = random.randint(0, i)
             self.cards[i], self.cards[r] = self.cards[r], self.cards[i]
+
+    def drawCard(self):
+        return self.cards.pop()
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.hand = []
+
+    def draw(self, deck):
+        self.hand.append(deck.drawCard())
+        return self
+
+    def showHand(self):
+        for card in self.hand:
+            card.show()
             
-        
+deck = Deck()
+deck.shuffle()
+
+Greg = Player("Greg")
+Greg.draw(deck)
+Greg.draw(deck)
+Greg.showHand()
 
 
 # Create bank class
